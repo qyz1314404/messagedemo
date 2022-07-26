c85618349de024d73b65297f42ea1aa27fab5050
Qiuyang <1206857991@qq.com>
2022-07-26 18:45:14 +0800

    once

---
+++ GitAmend/src/test/hello.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @description:
+ * @author: QiuYang
+ * @time: 2022/7/26
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
